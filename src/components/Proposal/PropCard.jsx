370441965be4ed5cb852e925849d79c5db624f36
Alex <42239536+RobertAnderson1985@users.noreply.github.com>
2024-02-26 16:50:43 +0300

    обновление результатов

---
+++ src/components/Proposal/PropCard.jsx
@@ -0,0 +1,8 @@
+const PropCard = ({name, text}) => {
+    return <div className="propcard">
+        <h4>{name}</h4>
+        <p>{text}</p>
+    </div>
+}
+
+export default PropCard;
--- src/components/Proposal/Proposal.jsx
+++ src/components/Proposal/Proposal.jsx
@@ -1,6 +1,9 @@
 import "./Proposal.css";
 import tagData from "../../assets/datas/tagdata.json";
+import propCardData from "../../assets/datas/propcarddata.json";
+import ilustration from "../../assets/images/ilustration_2.png";
 import Tag from './../Tag/Tag';
+import PropCard from "./PropCard";
 
 const Proposal = () => {
 
@@ -15,6 +18,23 @@ const Proposal = () => {
         <div className="tags">
             {arr.map((item, index) => <Tag value={item} key={index}/>)}
         </div>
+        <div className="proposal__startschool">
+            <h3>Запустите свою онлайн-школу</h3>
+            <p className="proposal__startschool-p">Или разверните корпоративный учебный портал уже сегодня.</p>
+            <h4>Цена 250 рублей за 1 ученика в месяц</h4>
+            <p className="proposal__startschool-p1">Все готово для запуска. Попробуйте 7-дневный демо-доступ.</p>
+            <button>ПОПРОБОВАТЬ БЕСПЛАТНО</button>
+        </div>
+        <div className="proposal__foryou">
+            <div className="proposal__foryou-container">
+                <h3>Точно подойдет для:</h3>
+                {propCardData.map((item, index) => 
+                    <PropCard name={item.name} text={item.text} />
+                )}
+            </div>
+            <img src={ilustration} alt="" />
+        </div>
+
     </section>
 }
 
