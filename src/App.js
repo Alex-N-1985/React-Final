839e7d5d21e6de3c812858b8f515c10de085c4c6
Alex <42239536+RobertAnderson1985@users.noreply.github.com>
2024-02-28 23:58:05 +0300

    обновление результатов

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import {Route, Routes} from 'react-router';
 import Home from './pages/Home';
+
+
 import Header from './components/Header/header';
 import Footer from './components/Footer/Footer';
+import Possibs from './pages/Possibs';
 
 function App() {
   return <div className='page'>
     <Header/>
     <Routes>
       <Route path="/" element={<Home/>} />
+      <Route path="/possibs" element={<Possibs/>} />
     </Routes>
     <Footer/>
   </div>;
---
+++ src/pages/Possibs.jsx
@@ -0,0 +1,10 @@
+import "../App.css";
+import Possibilities from './../components/Possibilities/Possibilities';
+
+const Possibs = () => {
+    return <div className="App">
+        <Possibilities/>
+    </div>
+}
+
+export default Possibs;
