5c0548ede368c97d7632071a997093b9220471a0
Alex <42239536+RobertAnderson1985@users.noreply.github.com>
2024-03-02 23:52:58 +0300

    обновление результатов

--- src/components/weather/weather.jsx
+++ src/components/weather/weather.jsx
@@ -4,19 +4,21 @@ import WeatherCard from './weathercard';
 
 const Weather = () => {
     const [cityName, setCityName] = useState("");
-    const [weatherData, setWeatherData] = useState([]);
+    const [weatherData, setWeatherData] = useState({});
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Алчевск&appid=f5b7f29b16e197c2ad71a56430df4512&units=metric&lang=RU";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Ростов&appid=f5b7f29b16e197c2ad71a56430df4512&units=metric&lang=RU";
+
 
     useEffect(() => {
         fetch(url).then((response) => {
             return response.json();
-        }).then((data) => {
-            setWeatherData(data);
-        });
+        }).then((result) => {
+            setWeatherData(result);
+        }).catch((error) => {console.log(error)});
     }, []);
-
+    
     console.log(weatherData);
+    console.log(Object.keys(weatherData).length);
 
     return <section className="weather">
         <h2>Погода</h2>
@@ -26,12 +28,15 @@ const Weather = () => {
             <button>Выбрать</button>
         </div>
         <div className="weather__container"> 
-            {/* <WeatherCard 
-                city={weatherData.name}
-                temperature={weatherData.main.temp}
-                cloudiness={weatherData.weather[0].description}
-                pressure={weatherData.main.pressure}
-            /> */}
+            {
+                Object.keys(weatherData).length === 0 ? "Ошибка 200" : <WeatherCard 
+                city={weatherData.name} 
+                temperature={weatherData.main.temp} 
+                cloudiness={weatherData.weather[0].description} 
+                pressure={weatherData.main.pressure} 
+                icon={weatherData.weather[0].icon}
+                /> 
+            }
         </div>
     </section>
 }
--- src/components/weather/weathercard.jsx
+++ src/components/weather/weathercard.jsx
@@ -1,17 +1,13 @@
 import "./weather.css";
 
-const WeatherCard = ({
-    city, 
-    temperature, 
-    cloudiness, 
-    pressure
-}) => {
+const WeatherCard = ({ city, temperature, cloudiness, pressure, icon }) => {
     return <div className="weather__card">
         <h3>{city}</h3>
         <div className="weather__card-data">
             <p>Температура: {temperature}</p>
-            <p>Облачность: {cloudiness}</p>
             <p>Атмосферное давление: {pressure}</p>
+            <p>Облачность: {cloudiness} <br/><img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} alt={cloudiness} /></p>
+            
         </div>
     </div>
 }
