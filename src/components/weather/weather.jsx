dcb67eb50796930c93fabffcebc665a813701394
Alex <42239536+RobertAnderson1985@users.noreply.github.com>
2024-03-03 16:15:28 +0300

    обновление результатов

--- src/components/weather/weather.jsx
+++ src/components/weather/weather.jsx
@@ -3,19 +3,19 @@ import "./weather.css";
 import WeatherCard from './weathercard';
 
 const Weather = () => {
-    const [cityName, setCityName] = useState("");
+    const [cityName, setCityName] = useState("Москва");
+    const [inputData, setInputData] = useState("Москва");
     const [weatherData, setWeatherData] = useState({});
-
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Ростов&appid=f5b7f29b16e197c2ad71a56430df4512&units=metric&lang=RU";
 
 
     useEffect(() => {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=f5b7f29b16e197c2ad71a56430df4512&units=metric&lang=RU`;
         fetch(url).then((response) => {
             return response.json();
         }).then((result) => {
             setWeatherData(result);
         }).catch((error) => {console.log(error)});
-    }, []);
+    }, [cityName]);
     
     console.log(weatherData);
     console.log(Object.keys(weatherData).length);
@@ -23,19 +23,19 @@ const Weather = () => {
     return <section className="weather">
         <h2>Погода</h2>
         <div className="weather__input">
-            <input type="text" name = "city" placeholder="Город" value={cityName} 
-            onChange={(e) => setCityName(e.target.value)} />
-            <button>Выбрать</button>
+            <input type="text" name = "city" placeholder="Город" value={inputData} 
+            onChange={(e) => setInputData(e.target.value)} />
+            <button onClick={(e) => {setCityName(inputData)}}>Выбрать</button>
         </div>
         <div className="weather__container"> 
             {
-                Object.keys(weatherData).length === 0 ? "Ошибка 200" : <WeatherCard 
+                Object.keys(weatherData).length === 0  ? <p>Поиск данных...</p> : Object.keys(weatherData).length > 2 ? <WeatherCard 
                 city={weatherData.name} 
                 temperature={weatherData.main.temp} 
                 cloudiness={weatherData.weather[0].description} 
                 pressure={weatherData.main.pressure} 
                 icon={weatherData.weather[0].icon}
-                /> 
+                /> : <p>Данные не найдены.</p>
             }
         </div>
     </section>
